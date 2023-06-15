f5bf2a52f2acc23d1fe526290f131b0e60364faa
ChrisRamFrontEnd <pheonixking56885@gmail.com>
2023-06-15 14:30:20 -0400

    add hover property and functionality

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ for(let i = 0; i < 16; i++){
     for(let j = 0; j< 16; j++){
         const square = document.createElement('div');
         square.style.cssText = 'width: 10px; height: 10px; background-color: grey';
-        square.addEventListener('hover',()=>{
-
+        square.addEventListener('mouseover',()=>{
+            square.style.backgroundColor = 'black';
         });
         container.appendChild(square);
     }
