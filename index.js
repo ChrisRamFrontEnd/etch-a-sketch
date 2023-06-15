a9bee4580faa42e5a5b593d29bbb4eb49ad26663
ChrisRamFrontEnd <pheonixking56885@gmail.com>
2023-06-15 14:13:56 -0400

    Change div size

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const container = document.querySelector('.container');
 for(let i = 0; i < 16; i++){
     for(let j = 0; j< 16; j++){
         const square = document.createElement('div');
-        square.style.cssText = 'width: 100px; height: 100px; background-color: grey';
+        square.style.cssText = 'width: 50px; height: 50px; background-color: grey';
         container.appendChild(square);
     }
 }
